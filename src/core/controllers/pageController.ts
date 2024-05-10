96b4363029be2e377e3d08b7914619ada6ee6bca
RATIU5 <jcmemmott20@outlook.com>
2024-05-09 21:34:07 -0600

    add: update page fn wip

--- src/core/controllers/pageController.ts
+++ src/core/controllers/pageController.ts
@@ -1,9 +1,25 @@
 import sql from "../utils/dbConnection";
 
+// TODO: Replace generic Errors with custom errors
+
+/**
+ * Get the id, slug, and parent_id for all the pages
+ * @returns an array of all the pages
+ */
+export async function getExistingPages() {
+  const res = await sql`SELECT id, slug, parent_id FROM page`;
+  return res.map((page) => ({
+    id: page.id,
+    parentId: page.parent_id,
+    slug: page.slug,
+  }));
+}
+
 /**
  * Create a new page and store it in the database
  * @param param The config object
  * @returns whether the page was created successfully or not
+ * @throws an error if the slug is empty, contains invalid characters, or already exists
  */
 export async function createPage({
   slug,
@@ -13,12 +29,21 @@ export async function createPage({
   parentId: number | null;
 }): Promise<boolean> {
   let status = false;
-  if (slug.trim() === "") {
-    console.log("Slug is required");
-    return status;
-  }
-
   try {
+    if (slug.trim() === "") {
+      throw new Error("Slug cannot be empty");
+    }
+
+    // Convert the slug to lowercase and replace spaces with hyphens
+    slug = slug.trim().replace(/\s+/g, "-").toLowerCase();
+
+    // Slug can only contain lowercase alphanumeric characters and hyphens
+    if (/^[a-z0-9]+(?:-[a-z0-9]+)*$/.test(slug) === false) {
+      throw new Error(
+        "Slug can only contain lowercase alphanumeric characters and hyphens"
+      );
+    }
+
     // Cannot have duplicate slugs for the same parent
     let existingPages;
     if (parentId !== null) {
@@ -42,15 +67,42 @@ export async function createPage({
   return status;
 }
 
-export async function getExistingPages() {
-  const res = await sql`SELECT id, slug, parent_id FROM page`;
-  return res.map((page) => ({
-    id: page.id,
-    parentId: page.parent_id,
-    slug: page.slug,
-  }));
+export type PageColumns = {
+  id: number;
+  slug: string;
+  parent_id: number | null;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
+  admin_name: string | null;
+  meta_id: number | null;
+  published_at: Date | null;
+};
+
+export type FieldsArray = Array<keyof PageColumns>;
+
+/**
+ * Read all pages with the specified fields
+ * @param param0 read pages with the specified fields
+ * @returns an array of pages
+ * @throws an error if the fields are invalid
+ */
+export async function readPages({ fields }: { fields: FieldsArray }) {
+  let res = {};
+  try {
+    res = await sql`SELECT ${sql(fields)} FROM page`;
+  } catch (e) {
+    console.error(e);
+  }
+  return res as PageColumns[];
 }
 
+/**
+ * Delete a page by its id
+ * @param id the id of the page to delete
+ * @returns whether the page was deleted successfully or not
+ * @throws an error if the page has children
+ */
 export async function deletePage(id: number) {
   let status = false;
   try {
@@ -59,6 +111,28 @@ export async function deletePage(id: number) {
       throw new Error("Cannot delete a page with children");
     }
     await sql`DELETE FROM page WHERE id = ${id}`;
+    status = true;
+  } catch (e) {
+    console.error(e);
+  }
+  return status;
+}
+
+export async function updatePage({
+  id,
+  fields,
+}: {
+  id: number;
+  fields: Partial<PageColumns>;
+}) {
+  let status = false;
+  try {
+    if (Object.keys(fields).length === 0) {
+      status = true;
+      return status;
+    }
+
+    await sql`UPDATE page SET ${sql(fields)} WHERE id = ${id}`;
     status = true;
   } catch (e) {
     console.error(e);
